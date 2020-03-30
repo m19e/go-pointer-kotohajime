075bdcbd2b8232a19cb5c24c2a946729d36f9721
m19e <nexus.me7290@gmail.com>
2020-03-31 03:45:14 +0900

    代入とアドレスへのアクセス

--- main.go
+++ main.go
@@ -9,15 +9,23 @@ type Person struct {
 }
 
 func main() {
-	// ポインタ型の変数を定義
-	// pがポインタ変数
-	// *Personポインタ型
-	var p *Person
-
-	p = &Person{
+	p := Person{
 		Name: "果穂",
 		Age:  12,
 	}
-	
-	fmt.Printf("変数pに格納されているアドレス :%p\n", p)
+	fmt.Printf("最初のp :%+v\n", p)
+
+	p2 := p
+	p2.Name = "夏葉"
+	p2.Age = 20
+	// Not p
+	fmt.Printf("p2で夏葉に書き換えを行なったはずのp :%+v\n", p)
+
+	// &pで*Person(Personのポインタ型)を生成する
+	// p3はpのアドレスが格納されている状態になる
+	p3 := &p
+	p3.Name = "夏葉"
+	p3.Age = 20
+
+	fmt.Printf("p3で夏葉に書き換えを行なったp :%+v\n", p)
 }
