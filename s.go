06759f2adf41d00cee3ef9b230f434e49d048e14
m19e <nexus.me7290@gmail.com>
2020-03-31 21:08:45 +0900

    関数に引数を渡す際、引数そのものではなく値がコピーされる

--- s.go
+++ s.go
@@ -3,11 +3,13 @@ package main // import "s"
 import "fmt"
 
 func Test(a string) {
-	fmt.Println("aのアドレス(関数内):", &a)
+	a = "園田" + a
+	fmt.Println("aの値(関数内):", a)
 }
 
 func main() {
 	a := "智代子"
-	fmt.Println("aのアドレス:", &a)
+	fmt.Println("Test()呼び出し前のaの値:", a)
 	Test(a)
+	fmt.Println("Test()呼び出し後のaの値:", a)
 }
