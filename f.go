e589ada02572d08cfe36b5cffc70cfea8bbc01fc
m19e <nexus.me7290@gmail.com>
2020-03-31 20:35:22 +0900

    破壊的代入

--- f.go
+++ f.go
@@ -14,4 +14,15 @@ func main() {
 
 	// namePointが指している変数は"*namePoint"のように"*"をつけて表す
 	fmt.Printf("namePoint :%v\n", *namePoint)
+
+	*namePoint = "凛世"
+
+	// *namePointは変数なので値を代入できます
+	fmt.Printf("*namePointに凛世を代入後の*namePoint :%v\n", *namePoint)
+
+	// 再代入してもnamePointに格納されている*string型の値(アドレス)自体は変わらない
+	fmt.Printf("*namePointに凛世を代入後のnamePoint :%v\n", namePoint)
+
+	// stringへのポインタである*string型の値(nameに格納されている値)を上書きしたのでnameの値も変更される
+	fmt.Printf("*namePointに凛世を代入後のname :%v\n", name)
 }
