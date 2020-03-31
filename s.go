09482b8f079d55792e60b45e41c3de4b11a7e917
m19e <nexus.me7290@gmail.com>
2020-03-31 21:21:50 +0900

    値渡しとポインタ渡し

--- s.go
+++ s.go
@@ -7,9 +7,16 @@ func Test(a string) {
 	fmt.Println("aの値(関数内):", a)
 }
 
+func TestPt(a *string) { // aはstringのポインタ変数になる
+	*a = "小糸" // aポインタが示す変数に入るstringを変更する
+	fmt.Println("aの値(関数内):", *a)
+}
+
 func main() {
 	a := "智代子"
 	fmt.Println("Test()呼び出し前のaの値:", a)
-	Test(a)
+
+	// 呼び出し時に引数としてアドレスを渡す
+	TestPt(&a)
 	fmt.Println("Test()呼び出し後のaの値:", a)
 }
